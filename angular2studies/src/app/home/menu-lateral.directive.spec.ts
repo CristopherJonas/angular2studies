faacb3aab67849f937a20aabb21ffafdda132935
CRISTOPHER\cristopher <cristopherjonasfaria@gmail.com>
2017-03-09 22:54:59 -0300

    remove directive

--- angular2studies/src/app/home/menu-lateral.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MenuLateralDirective } from './menu-lateral.directive';
-
-describe('MenuLateralDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MenuLateralDirective();
-    expect(directive).toBeTruthy();
-  });
-});
